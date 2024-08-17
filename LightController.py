ed8198576120302d52d7fcef50f6216d25499aa2
NivEz <niv.cyber@gmail.com>
2024-08-17 20:43:37 +0300

    executing wheel color task loop only if holding longer then 0.2s

--- LightController.py
+++ LightController.py
@@ -21,8 +21,10 @@ class LightController:
         self.my_controller = None
         self.last_timestamp = time()
         self.currently_held_button = None
+        self.currently_held_button_press_timestamp = None
         self.wheel_color_loop = None
-        self.additive_y = None
+        self.is_wheel_color_mode = False
+        self.additive_y = 0
 
     async def start(self):
         pygame.init()
@@ -45,39 +47,55 @@ class LightController:
 
     async def handle_joystick_controls(self, event):
         if event.type == pygame.JOYBUTTONUP:
-            self.currently_held_button = None
-            self.wheel_color_loop.cancel()
+            if event.button == self.currently_held_button:
+                # use timestamp difference to distinguish between holding and a normal press
+                self.currently_held_button = None
+                if self.is_wheel_color_mode:
+                    print("Stopping wheel color loop")
+                    self.wheel_color_loop.cancel()
+                    self.is_wheel_color_mode = False
+                    return
             bulb_pwr = self.bulb.get_state()['pwr']
             if event.button == xbox360_controller.START:
                 self.bulb.set_state(pwr=int(not bulb_pwr))
             if not bulb_pwr:
                 return
-            if event.button == xbox360_controller.BACK:
-                bulb_color_mode = self.bulb.get_state()['bulb_colormode']
-                self.bulb.set_state(bulb_colormode=int(not bulb_color_mode), brightness=50)
-            if event.button == xbox360_controller.A:
-                self.bulb.set_state(red=0, green=100, blue=0)
-            if event.button == xbox360_controller.B:
-                self.bulb.set_state(red=100, green=0, blue=0)
-            if event.button == xbox360_controller.X:
-                self.bulb.set_state(red=0, green=0, blue=100)
-            if event.button == xbox360_controller.Y:
-                self.bulb.set_state(red=255, green=140, blue=0)
+            match event.button:
+                case xbox360_controller.BACK:
+                    bulb_color_mode = self.bulb.get_state()['bulb_colormode']
+                    self.bulb.set_state(bulb_colormode=int(not bulb_color_mode), brightness=50)
+                case xbox360_controller.A:
+                    self.bulb.set_state(red=0, green=100, blue=0)
+                case xbox360_controller.B:
+                    self.bulb.set_state(red=100, green=0, blue=0)
+                case xbox360_controller.X:
+                    self.bulb.set_state(red=0, green=0, blue=100)
+                case xbox360_controller.Y:
+                    self.bulb.set_state(red=255, green=140, blue=0)
         elif event.type == pygame.JOYBUTTONDOWN:
             if event.button in [xbox360_controller.B, xbox360_controller.A, xbox360_controller.X]:
                 self.currently_held_button = event.button
-                self.wheel_color_loop = asyncio.create_task(self.create_wheel_color_loop())
+                self.currently_held_button_press_timestamp = time()
         elif event.type == pygame.JOYAXISMOTION:
+            if self.currently_held_button is None:
+                return
             now = time()
-            # sort of throttling to prevent from the bulb to be flooded
+            # sort of throttling to prevent too much tasks / calculations on short time
             if (now - self.last_timestamp) < 0.05:
                 return
-            left_x, left_y = self.my_controller.get_left_stick()
-            y = (-1 * left_y) * 10
-            self.additive_y = y
+            # check if the button is held enough time (0.2s) and then execute the wheel color task loop
+            if now - self.currently_held_button_press_timestamp > 0.2 and not self.is_wheel_color_mode:
+                self.wheel_color_loop = asyncio.create_task(self.create_wheel_color_loop())
+                self.is_wheel_color_mode = True
+            if not self.is_wheel_color_mode:
+                return
+            # only if got here calculate the new value
+            _, left_y = self.my_controller.get_left_stick()
+            self.additive_y = (-1 * left_y) * 15
             self.last_timestamp = now
 
     async def create_wheel_color_loop(self):
+        print("Starting wheel color loop")
         while True:
             await asyncio.sleep(0.1)
             bulb_state = self.bulb.get_state()
